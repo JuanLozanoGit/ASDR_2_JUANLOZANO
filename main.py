5e3583100e1b99b7975f4237fc7ba70345ae8984
JuanLozanoGit <juancamilolozanocortesunal@gmail.com>
2026-04-13 15:57:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from gramatica import EJERCICIO_1, EJERCICIO_2, EJERCICIO_3
 from solver import Procesador
 
 if __name__ == "__main__":
-    # Lista de ejercicios a procesar
-    ejes = [
+    ejercicios = [
         ("EJERCICIO 1", EJERCICIO_1),
         ("EJERCICIO 2", EJERCICIO_2),
         ("EJERCICIO 3", EJERCICIO_3)
     ]
     
-    for nombre, datos in ejes:
-        app = Procesador(nombre, datos)
-        app.ejecutar_analisis_completo()
+    for nombre, datos in ejercicios:
+        analizador = Procesador(nombre, datos)
+        analizador.ejecutar_analisis_completo()
