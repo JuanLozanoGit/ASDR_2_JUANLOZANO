2309997d4573d6a477d334b1aa73482b9937b62e
JuanLozanoGit <juancamilolozanocortesunal@gmail.com>
2026-04-13 15:41:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from gramatica import EJERCICIO_1, EJERCICIO_2, EJERCICIO_3
-from procesador import ProcesadorGramatica
+from solver import ProcesadorGramatica
 
-def ejecutar_revision_completa(num, gramatica):
-    print("\n" + "#"*70)
-    print(f"      INICIANDO REVISIÓN COMPLETA DEL EJERCICIO {num}")
-    print("#"*70)
+def ejecutar_laboratorio(num, datos):
+    print("\n" + "="*80)
+    print(f"   REVISIÓN TÉCNICA PASO A PASO: EJERCICIO {num}")
+    print("="*80)
     
-    p = ProcesadorGramatica(f"EJ{num}", gramatica)
-    p.revisar_recursividad()
-    p.calcular_teoria()
-    p.revisar_predict_y_ll1()
-    p.generar_esqueleto_asdr()
+    p = ProcesadorGramatica(f"EJ{num}", datos)
+    p.eliminar_recursividad()
+    p.calcular_conjuntos()
+    p.analizar_predict_y_ll1()
+    p.imprimir_asdr()
 
 if __name__ == "__main__":
     ejercicios = [EJERCICIO_1, EJERCICIO_2, EJERCICIO_3]
     for i, g in enumerate(ejercicios, 1):
-        ejecutar_revision_completa(i, g)
+        ejecutar_laboratorio(i, g)
